3936f4ac7bde2ea4d4541beda26ce4eeb83b3ad3
GCW <1018575921@qq.com>
2018-11-15 09:59:22 +0800

    修改了文档界面的兼容

--- src/app/document-page/document-page.component.ts
+++ src/app/document-page/document-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewEncapsulation  } from '@angular/core';
 
 @Component({
   selector: 'app-document-page',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './document-page.component.html',
   styleUrls: ['./document-page.component.css']
 })
